770d2a3d78f867337b3c4f855903f62551d44bc0
James Borkowski <v-jborkowski@expedia.com>
2014-02-28 16:43:46 +0000

    Checking in last nights work making the headings wrap lines based on the <textWidth> config option

--- src/main/java/com/buildmonkey/maven/plugins/echo/EchoMojo.java
+++ src/main/java/com/buildmonkey/maven/plugins/echo/EchoMojo.java
@@ -37,138 +37,184 @@ import com.github.lalyos.jfiglet.FigletFont;
 @Mojo(name="echo", requiresProject = true, threadSafe=true)
 public class EchoMojo extends AbstractEchoPlugIn {
 
-  /**
-   * The different levels 
-   * which exist to printout the echos.
-   */
-  public enum LogLevels {
-	INFO,
-	WARNING,
-  	ERROR,
-  	DEBUG,
-  }
+	public static int getCharacterCountLongestLine(String toParse){
+		//Parses multi-line text and returns a character count of the longest line
+		Scanner Scanner = new Scanner( toParse );
+		int lines=0;
+		int characters=0;
+		int maxCharacters=0;
+		int wrapCharacters=80;
+		while(Scanner.hasNextLine()){
+			String line = Scanner.nextLine();
+			lines++;
+			characters+=line.length();
+			if(maxCharacters<line.length()){
+				maxCharacters = line.length();
+			}
+		}
+		return maxCharacters;
+	}
 	
-  /**
-   * This will define in which logging level 
-   * the given messages will be printed out.
-   * Their exist the following logging levels:
-   * <ul>
-   * <li>INFO</li>
-   * <li>WARNING</li>
-   * <li>ERROR<li>
-   * <li>DEBUG<li>
-   * </ul>
-   * 
-   */
-  @Parameter(defaultValue="INFO", required=true)
-  private LogLevels logLevel;
-  
+	public void renderMavenOutputScanner(Scanner scanner){
+		switch ( logLevel ) 
+		{
+		case DEBUG:
+			while (scanner.hasNextLine()) 
+			{
+				String line = scanner.nextLine();
+				// process the line
+				getLog().debug( line );
+			}
+			scanner.close();
+			break;
+		case ERROR:
+			while (scanner.hasNextLine()) 
+			{
+				String line = scanner.nextLine();
+				// process the line
+				getLog().error( line );
+			}
+			scanner.close();
+			break;
+		case INFO:
+			while (scanner.hasNextLine()) 
+			{
+				String line = scanner.nextLine();
+				// process the line
+				getLog().info( line );
+			}
+			scanner.close();
+			break;
+		case WARNING:
+			while (scanner.hasNextLine()) 
+			{
+				String line = scanner.nextLine();
+				// process the line
+				getLog().warn( line );
+			}
+			scanner.close();
+			break;
+		}
+	}
 
-  /**
-   * This will turn on or off the LARGE text heading that displays
-   * the text set with the 'default-text' property, or, if not set
-   * defaults to 'project.artifactId' 'project.version' and uses
-   * the jFiglet library to display it as ASCII Art.
-   * 
-   */
-  @Parameter(defaultValue="false", required=false)
-  private boolean headingEnable;
-	
-  /**
-   * This will set the text to use for the heading,
-   * the given message will be printed out as a heading using
-   * the jFiglet library to display it as ASCII Art.
-   * 
-   */
-  @Parameter(defaultValue="${project.artifactId} ${project.version}", required=false)
-  private String headingText;
-	
-  /**
-   * The messages you would like to print out.
-   * 
-   * <pre>
-   *    ..
-   *    &lt;configuration&gt;
-   *      &lt;echos&gt;
-   *        &lt;echo&gt;Message Line Nr. 1&lt;/echo&gt;
-   *        &lt;echo&gt;Message Line Nr. 2&lt;/echo&gt;
-   *      &lt;/echos&gt;
-   *    &lt;/configuration&gt;
-   *    ..
-   * </pre>
-   */
-  @Parameter(required = true)
-  private List<String> echos;
+	/**
+	 * The different levels 
+	 * which exist to printout the echos.
+	 */
+	public enum LogLevels {
+		INFO,
+		WARNING,
+		ERROR,
+		DEBUG,
+	}
 
-    /*
-     * (non-Javadoc)
-     * @see org.apache.maven.plugin.Mojo#execute()
-     */
-    public void execute () throws MojoExecutionException
-    {
-      if ( headingEnable = true )
-      {
-        String asciiArt = FigletFont.convertOneLine( headingText );
-        Scanner scanner = new Scanner( asciiArt );
-        switch ( logLevel ) 
-        {
-            case DEBUG:
-                while (scanner.hasNextLine()) 
-                {
-                    String line = scanner.nextLine();
-                    // process the line
-                    getLog().debug( line );
-                }
-                scanner.close();
-                break;
-            case ERROR:
-                while (scanner.hasNextLine()) 
-                {
-                    String line = scanner.nextLine();
-                    // process the line
-                    getLog().error( line );
-                }
-                scanner.close();
-                break;
-            case INFO:
-                while (scanner.hasNextLine()) 
-                {
-                    String line = scanner.nextLine();
-                    // process the line
-                    getLog().info( line );
-                }
-                scanner.close();
-                break;
-            case WARNING:
-                while (scanner.hasNextLine()) 
-                {
-                    String line = scanner.nextLine();
-                    // process the line
-                    getLog().warn( line );
-                }
-                scanner.close();
-                break;
-        }
-      }
+	/**
+	 * This will define in which logging level 
+	 * the given messages will be printed out.
+	 * Their exist the following logging levels:
+	 * <ul>
+	 * <li>INFO</li>
+	 * <li>WARNING</li>
+	 * <li>ERROR<li>
+	 * <li>DEBUG<li>
+	 * </ul>
+	 * 
+	 */
+	@Parameter(defaultValue="INFO", required=true)
+	private LogLevels logLevel;
 
-      for ( String item : echos )
-      {
-          switch ( logLevel ) 
-          {
-              case DEBUG:
-                  getLog ( ).debug ( item );
-                  break;
-              case ERROR:
-                  getLog ( ).error ( item );
-                  break;
-              case INFO:
-                  getLog ( ).info ( item );
-                  break;
-              case WARNING:
-                  getLog ( ).warn ( item );
-                  break;
-          }
-       }
-    }
+
+	/**
+	 * This will turn on or off the LARGE text heading that displays
+	 * the text set with the 'default-text' property, or, if not set
+	 * defaults to 'project.artifactId' 'project.version' and uses
+	 * the jFiglet library to display it as ASCII Art.
+	 * 
+	 */
+	@Parameter(defaultValue="false", required=false)
+	private boolean headingEnable;
+
+	/**
+	 * This will set the text width in characters before the text being
+	 * displayed be wrapped to the next line.
+	 * 
+	 */
+	@Parameter(defaultValue="120", required=false)
+	private int textWidth;
+
+	/**
+	 * This will set the text to use for the heading,
+	 * the given message will be printed out as a heading using
+	 * the jFiglet library to display it as ASCII Art.
+	 * 
+	 */
+	@Parameter(defaultValue="${project.artifactId} ${project.version}", required=false)
+	private String headingText;
+
+	/**
+	 * The messages you would like to print out.
+	 * 
+	 * <pre>
+	 *    ..
+	 *    &lt;configuration&gt;
+	 *      &lt;echos&gt;
+	 *        &lt;echo&gt;Message Line Nr. 1&lt;/echo&gt;
+	 *        &lt;echo&gt;Message Line Nr. 2&lt;/echo&gt;
+	 *      &lt;/echos&gt;
+	 *    &lt;/configuration&gt;
+	 *    ..
+	 * </pre>
+	 */
+	@Parameter(required = true)
+	private List<String> echos;
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.apache.maven.plugin.Mojo#execute()
+	 */
+	public void execute () throws MojoExecutionException
+	{
+		if ( headingEnable = true )
+		{
+			String asciiArt = FigletFont.convertOneLine( headingText );
+			Scanner scanner = new Scanner( asciiArt );
+			String headingTextToDo = "";
+			while ( getCharacterCountLongestLine( asciiArt ) > textWidth ){
+				while ( getCharacterCountLongestLine( asciiArt ) > textWidth ){
+					headingTextToDo=Character.toString(headingText.charAt(headingText.length()-1))+headingTextToDo;
+					headingText=headingText.substring(0, headingText.length()-1);
+					asciiArt = FigletFont.convertOneLine( headingText );
+					scanner = new Scanner( asciiArt );
+				}
+				renderMavenOutputScanner(scanner);
+				headingText=headingTextToDo;
+				headingTextToDo="";
+				asciiArt = FigletFont.convertOneLine( headingText );
+				scanner = new Scanner( asciiArt );
+			}
+			renderMavenOutputScanner(scanner);
+		}
+		
+
+
+		for ( String item : echos )
+		{
+			switch ( logLevel ) 
+			{
+			case DEBUG:
+				getLog ( ).debug ( item );
+				break;
+			case ERROR:
+				getLog ( ).error ( item );
+				break;
+			case INFO:
+				getLog ( ).info ( item );
+				break;
+			case WARNING:
+				getLog ( ).warn ( item );
+				break;
+			}
+		}
+	}
 
 }
